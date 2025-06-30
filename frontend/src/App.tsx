83f82551e6915fcd0ebd7fb07af5cf3549bdb41f
ShifatJahanShifa <bsse1301@iiit.du.ac.bd>
2025-06-30 17:27:32 +0600

    feat: init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-amber-200">
+        <h1> Hello Cefalo Travel connect</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
